e645afa0ede7b18b1d25f7cdaeaa2648d1499cad
fikrimulya23 <fikrimulyapermana00@gmail.com>
2022-03-07 17:58:15 +0700

    update file name iklanController

--- app/Model/Provinsi.php
+++ app/Model/Provinsi.php
@@ -6,10 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Provinsi extends Model
 {
-    //
     protected $fillable = [
         'nama_provinsi'
     ];
+
+    // protected $with = ['kabupaten'];
 
     public function kabupaten()
     {
